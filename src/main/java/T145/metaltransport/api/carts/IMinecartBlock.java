64734d259efce26b52e81cb5d2c3a182e588ce7b
T145 <T145@protonmail.com>
2019-06-07 13:06:19 -0400

    Updated minecart block API
    TODO:
    - Render blocks like pressure plates & carpets flatly, along the entire bottom of the cart

--- src/main/java/T145/metaltransport/api/carts/IMinecartBlock.java
+++ src/main/java/T145/metaltransport/api/carts/IMinecartBlock.java
@@ -6,5 +6,22 @@ import net.minecraft.item.ItemStack;
 
 public interface IMinecartBlock {
 
+	/*
+	 * Returns the block state rendered by a metal minecart.
+	 * 
+	 * @param cart Instance of the minecart
+	 * 
+	 * @param data ItemStack representing the initially added stack
+	 * 
+	 * @return Block state to be rendered
+	 */
 	public IBlockState getDisplayState(EntityMinecartEmpty cart, ItemStack data);
+
+	/*
+	 * Whether or not a block should be flatly rendered in a metal minecart.
+	 * Blocks rendered like this would be pressure plates & carpets.
+	 * 
+	 * @return Whether or not this block should be rendered flatly.
+	 */
+	public boolean shouldRenderFlatly();
 }

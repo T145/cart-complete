3f741dd18b56735129c28861f683a545969051d3
T145 <T145@protonmail.com>
2019-06-11 13:18:50 -0400

    Added lamp cart behavior

--- src/main/java/T145/metaltransport/core/MetalTransport.java
+++ src/main/java/T145/metaltransport/core/MetalTransport.java
@@ -15,6 +15,7 @@ import T145.metaltransport.client.render.entities.RenderMetalMinecart;
 import T145.metaltransport.entities.EntityMetalMinecart;
 import T145.metaltransport.entities.behaviors.EnderChestBehavior;
 import T145.metaltransport.entities.behaviors.FurnaceBehavior;
+import T145.metaltransport.entities.behaviors.LampBehavior;
 import T145.metaltransport.entities.behaviors.MobSpawnerBehavior;
 import T145.metaltransport.entities.behaviors.SimpleGuiBehavior;
 import T145.metaltransport.entities.behaviors.TNTBehavior;
@@ -107,6 +108,7 @@ public class MetalTransport {
 		CartBehaviorRegistry.register(new EnderChestBehavior());
 		CartBehaviorRegistry.register(new MobSpawnerBehavior());
 		CartBehaviorRegistry.register(new TNTBehavior());
+		CartBehaviorRegistry.register(new LampBehavior());
 	}
 
 	@SubscribeEvent

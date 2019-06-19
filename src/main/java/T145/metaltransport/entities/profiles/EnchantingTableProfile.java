4b3bef092b9d22cb93d09298d5778fd5c9b8afc0
T145 <T145@protonmail.com>
2019-06-19 14:29:30 -0400

    Temporarily disable render attempts

--- src/main/java/T145/metaltransport/entities/profiles/EnchantingTableProfile.java
+++ src/main/java/T145/metaltransport/entities/profiles/EnchantingTableProfile.java
@@ -1,11 +1,8 @@
 package T145.metaltransport.entities.profiles;
 
 import T145.metaltransport.api.profiles.ICartProfileFactory;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.entity.item.EntityMinecart;
 import net.minecraft.item.ItemStack;
-import net.minecraft.tileentity.TileEntityEnchantmentTable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -20,23 +17,11 @@ public class EnchantingTableProfile extends SimpleGuiProfile {
 		}
 	}
 
-	@SideOnly(Side.CLIENT)
-	public final TileEntityEnchantmentTable table = new TileEntityEnchantmentTable();
-
 	public EnchantingTableProfile(EntityMinecart cart) {
 		super(cart);
-		table.setWorld(Minecraft.getMinecraft().world);
-		table.setPos(BlockPos.ORIGIN);
 	}
 
 	@SideOnly(Side.CLIENT)
 	@Override
-	public void render(BlockPos pos, ItemStack stack, float partialTicks) {
-		if (table.getPos() != pos) {
-			table.setPos(pos);
-		}
-
-		table.update();
-		TileEntityRendererDispatcher.instance.render(table, 0, 0, 0, 0, partialTicks);
-	}
+	public void render(BlockPos pos, ItemStack stack, float partialTicks) {}
 }

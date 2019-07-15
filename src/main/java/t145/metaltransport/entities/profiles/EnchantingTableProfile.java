552633f0df202d1940e03a85c131a934ae9610d4
T145 <T145@protonmail.com>
2019-07-15 14:13:33 -0400

    Cleaned up the enchantment table profile

--- src/main/java/t145/metaltransport/entities/profiles/EnchantingTableProfile.java
+++ src/main/java/t145/metaltransport/entities/profiles/EnchantingTableProfile.java
@@ -5,23 +5,21 @@ import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.client.gui.GuiEnchantment;
-import net.minecraft.client.model.ModelBook;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.Render;
-import net.minecraft.client.renderer.tileentity.TileEntityEnchantmentTableRenderer;
+import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityMinecart;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.ContainerEnchantment;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntityEnchantmentTable;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.IWorldNameable;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeHooks;
@@ -33,7 +31,7 @@ import t145.metaltransport.api.profiles.IProfileFactory;
 import t145.metaltransport.api.profiles.IUniversalProfile;
 import t145.metaltransport.entities.EntityMetalCart;
 
-public class EnchantingTableProfile implements IUniversalProfile, IWorldNameable {
+public class EnchantingTableProfile extends TileEntityEnchantmentTable implements IUniversalProfile, IWorldNameable {
 
 	public static class ProfileFactoryEnchantingTable implements IProfileFactory, IGuiHandler {
 
@@ -80,95 +78,30 @@ public class EnchantingTableProfile implements IUniversalProfile, IWorldNameable
 		}
 	}
 
-	private static final Random rand = new Random();
-	private final ModelBook book = new ModelBook();
 	private final EntityMinecart cart;
-	public int tickCount;
-	public float pageFlip;
-	public float pageFlipPrev;
-	public float flipT;
-	public float flipA;
-	public float bookSpread;
-	public float bookSpreadPrev;
-	public float bookRotation;
-	public float bookRotationPrev;
-	public float tRot;
 
 	public EnchantingTableProfile(EntityMinecart cart) {
 		this.cart = cart;
+		this.world = cart.world;
 	}
 
-	private float calcBookRotation(float diff) {
-		float result;
-
-		for (result = diff; result >= Math.PI; result -= (Math.PI * 2F));
-
-		while (result < -Math.PI) {
-			result += (Math.PI * 2F);
-		}
-
-		return result;
-	}
-
-	private EntityPlayer getClosePlayer(World world) {
-		return world.getClosestPlayer(cart.posX, cart.posY + 0.5D, cart.posZ, 3.0D, false);
+	@Override
+	public NBTTagCompound writeToNBT(NBTTagCompound tag) {
+		tag.setInteger("x", pos.getX());
+		tag.setInteger("y", pos.getY());
+		tag.setInteger("z", pos.getZ());
+		RegistryMT.LOG.info(tag);
+		return tag;
 	}
 
 	@Override
 	public void tick(World world, BlockPos pos) {
-		this.bookSpreadPrev = this.bookSpread;
-		this.bookRotationPrev = this.bookRotation;
-		EntityPlayer player = getClosePlayer(world);
-
-		if (player != null) {
-			double d0 = player.posX - cart.posX;
-			double d1 = player.posZ - cart.posZ;
-			this.tRot = (float) MathHelper.atan2(d1, d0);
-			this.bookSpread += 0.1F;
-
-			if (this.bookSpread < 0.5F || rand.nextInt(40) == 0) {
-				float f1 = this.flipT;
-
-				while (true) {
-					this.flipT += (rand.nextInt(4) - rand.nextInt(4));
-
-					if (f1 != this.flipT) {
-						break;
-					}
-				}
-			}
-		} else {
-			this.tRot += 0.02F;
-			this.bookSpread -= 0.1F;
-		}
-
-		while (this.bookRotation >= Math.PI) {
-			this.bookRotation -= (Math.PI * 2F);
-		}
-
-		while (this.bookRotation < -Math.PI) {
-			this.bookRotation += (Math.PI * 2F);
-		}
-
-		while (this.tRot >= Math.PI) {
-			this.tRot -= (Math.PI * 2F);
-		}
-
-		while (this.tRot < -Math.PI) {
-			this.tRot += (Math.PI * 2F);
-		}
-
-		this.bookRotation += calcBookRotation(this.tRot - this.bookRotation) * 0.4F;
-		this.bookSpread = MathHelper.clamp(this.bookSpread, 0.0F, 1.0F);
-		++this.tickCount;
-		this.pageFlipPrev = this.pageFlip;
-		float f = (this.flipT - this.pageFlip) * 0.4F;
-		float f3 = 0.2F;
-		f = MathHelper.clamp(f, -0.2F, 0.2F);
-		this.flipA += (f - this.flipA) * 0.9F;
-		this.pageFlip += this.flipA;
+		this.pos = cart.getPosition();
+		this.update();
 
 		if (world.isRemote && world.getTotalWorldTime() % 5L == 0L) {
+			Random rand = world.rand;
+
 			for (short i = -2; i <= 2; ++i) {
 				for (short j = -2; j <= 2; ++j) {
 					if (i > -2 && i < 2 && j == -1) {
@@ -194,11 +127,7 @@ public class EnchantingTableProfile implements IUniversalProfile, IWorldNameable
 									break;
 								}
 
-								world.spawnParticle(EnumParticleTypes.ENCHANTMENT_TABLE, pos.getX() + 0.5D,
-										pos.getY() + 2.0D, pos.getZ() + 0.5D,
-										i + rand.nextFloat() - 0.5D,
-										k - rand.nextFloat() - 1,
-										j + rand.nextFloat() - 0.5D);
+								world.spawnParticle(EnumParticleTypes.ENCHANTMENT_TABLE, pos.getX() + 0.5D, pos.getY() + 2.0D, pos.getZ() + 0.5D, i + rand.nextFloat() - 0.5D, k - rand.nextFloat() - 1, j + rand.nextFloat() - 0.5D);
 							}
 						}
 					}
@@ -217,70 +146,10 @@ public class EnchantingTableProfile implements IUniversalProfile, IWorldNameable
 		}
 	}
 
-	private float computeYRotation(EntityMinecart cart, float partialTicks) {
-		EntityPlayer player = getClosePlayer(cart.world);
-		float rot = 180F;
-
-		if (player != null) {
-		}
-
-		float f2 = bookRotationPrev + this.calcBookRotation(bookRotation - bookRotationPrev) * partialTicks;
-		return (float) (-f2 * (rot / Math.PI));
-	}
-
 	@SideOnly(Side.CLIENT)
 	@Override
 	public void render(Render renderer, EntityMinecart cart, ItemStack stack, float partialTicks) {
-		GlStateManager.pushMatrix();
-		GlStateManager.rotate(computeYRotation(cart, partialTicks), 0, 1, 0);
-		GlStateManager.translate(0, 0.65, 0);
-
-		float f = tickCount + partialTicks;
-		GlStateManager.translate(0, 0.1F + MathHelper.sin(f * 0.1F) * 0.01F, 0);
-		GlStateManager.rotate(80F, 0, 0, 1);
-		renderer.bindTexture(TileEntityEnchantmentTableRenderer.TEXTURE_BOOK);
-
-		float f3 = pageFlipPrev + (pageFlip - pageFlipPrev) * partialTicks + 0.25F;
-		float f4 = pageFlipPrev + (pageFlip - pageFlipPrev) * partialTicks + 0.75F;
-
-		f3 = (f3 - MathHelper.fastFloor(f3)) * 1.6F - 0.3F;
-		f4 = (f4 - MathHelper.fastFloor(f4)) * 1.6F - 0.3F;
-
-		if (f3 < 0.0F) {
-			f3 = 0.0F;
-		}
-
-		if (f4 < 0.0F) {
-			f4 = 0.0F;
-		}
-
-		if (f3 > 1.0F) {
-			f3 = 1.0F;
-		}
-
-		if (f4 > 1.0F) {
-			f4 = 1.0F;
-		}
-
-		float f5 = bookSpreadPrev + (bookSpread - bookSpreadPrev) * partialTicks;
-
-		GlStateManager.enableCull();
-		book.render(cart, f, f3, f4, f5, 0.0F, 0.0625F);
-		GlStateManager.popMatrix();
-	}
-
-	@Override
-	public String getName() {
-		return "container.enchant";
-	}
-
-	@Override
-	public boolean hasCustomName() {
-		return false;
-	}
-
-	@Override
-	public ITextComponent getDisplayName() {
-		return new TextComponentTranslation(this.getName(), new Object[0]);
+		GlStateManager.rotate(90F, 0, 1, 0);
+		TileEntityRendererDispatcher.instance.render(this, -0.5, -0.12, -0.5, partialTicks);
 	}
 }

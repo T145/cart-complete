13ae1f62e42aa718d75bc53907a56eb7c9a6809b
T145 <T145@protonmail.com>
2019-07-22 17:23:36 -0400

    Fixed dropper inventory insertion

--- src/main/java/t145/metaltransport/entities/profiles/DropperProfile.java
+++ src/main/java/t145/metaltransport/entities/profiles/DropperProfile.java
@@ -1,12 +1,13 @@
 package t145.metaltransport.entities.profiles;
 
-import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import net.minecraft.dispenser.BehaviorDefaultDispenseItem;
 import net.minecraft.dispenser.IBehaviorDispenseItem;
 import net.minecraft.entity.item.EntityMinecart;
 import net.minecraft.init.Blocks;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.ISidedInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
@@ -14,8 +15,12 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.wrapper.InvWrapper;
+import net.minecraftforge.items.wrapper.SidedInvWrapper;
+import t145.metaltransport.api.consts.RegistryMT;
 import t145.metaltransport.api.profiles.IProfileFactory;
 import t145.metaltransport.entities.EntityMetalCart;
+import t145.tbone.lib.ChestHelper;
 
 public class DropperProfile extends DispenserProfile {
 
@@ -39,58 +44,41 @@ public class DropperProfile extends DispenserProfile {
 		EnumFacing front = cart.getHorizontalFacing();
 		BlockPos pos = this.cart.getPosition().offset(front);
 		TileEntity te = world.getTileEntity(pos);
-		EnumFacing targetedSide = front.getOpposite();
 
-		// TODO: Fix crashing when the cap is null
-		if (te.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, targetedSide)) {
-			return te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, targetedSide);
+		if (te != null) {
+			EnumFacing targetedSide = front.getOpposite();
+
+			if (te.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, targetedSide)) {
+				return te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, targetedSide);
+			} else if (te instanceof ISidedInventory && targetedSide != null) {
+				return new SidedInvWrapper((ISidedInventory) te, targetedSide);
+			} else if (te instanceof IInventory) {
+				return new InvWrapper((IInventory) te);
+			}
 		}
 
 		return null;
 	}
 
-	public static ItemStack tryToInsertStack(final IItemHandler inv, final @Nonnull ItemStack stack) {
-		ItemStack result = stack.copy();
-
-		for (int slot = 0; slot < inv.getSlots(); ++slot) {
-			if (!inv.getStackInSlot(slot).isEmpty()) {
-				result = inv.insertItem(slot, result, false);
-
-				if (result.isEmpty()) {
-					return ItemStack.EMPTY;
-				}
-			}
-		}
-
-		for (int slot = 0; slot < inv.getSlots(); ++slot) {
-			result = inv.insertItem(slot, result, false);
-
-			if (result.isEmpty()) {
-				return ItemStack.EMPTY;
-			}
-		}
-
-		return result;
-	}
-
 	@Override
 	protected void dispenseStack(int slot, ItemStack stack) {
+		if (stack.isEmpty()) {
+			return;
+		}
+
 		IItemHandler handler = getInventoryOverYonder();
-		ItemStack slotStack;
+		ItemStack slotStack = ItemStack.EMPTY;
 
 		if (handler == null) {
 			slotStack = this.dropBehavior.dispense(new DispenserSource(), stack);
 		} else {
-			slotStack = tryToInsertStack(handler, stack);
-
-			if (slotStack.isEmpty()) {
-				slotStack = stack.copy();
-				slotStack.shrink(1);
-			} else {
-				slotStack = stack.copy();
-			}
+			slotStack = stack.copy();
+			slotStack.setCount(1);
+			ChestHelper.tryToInsertStack(handler, slotStack);
+			slotStack.setCount(stack.getCount() - 1);
 		}
 
+		cart.world.playEvent(1000, cart.getPosition(), 0);
 		this.dispenser.setInventorySlotContents(slot, slotStack);
 	}
 }
